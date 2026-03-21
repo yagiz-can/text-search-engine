b15ee1b81010e906d9c9f1a04f8a4f87c0cb1e2b
ebrarzcan <ebrarzercan@gmail.com>
2026-03-22 00:38:32 +0300

    Moduler yapi, coklu dosya destegi ve raporlama sistemi eklendi

--- text_search.c
+++ text_search.c
@@ -1,99 +1,146 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<time.h>
-#include<ctype.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-#define RENK_YAZI "\x1b[36m"   // Cyan renkli metin
-#define RENK_RESET "\x1b[0m"   // Renk sıfırla
+#define RENK_YAZI "\x1b[36m" // Cyan renkli metin
+#define RENK_RESET "\x1b[0m" // Renk sıfırla
 
 // Küçük harfe çevirme fonksiyonu
-void kucuk_harf(char *str){
-    for(int i=0; str[i]; i++){
-        str[i] = tolower(str[i]);
-    }
+void kucuk_harf(char *str) {
+  for (int i = 0; str[i]; i++) {
+    str[i] = tolower(str[i]);
+  }
 }
 
-//Kelime arama fonksiyonu
-int kelime_bulma(char dosya_adi[100], char kelime[100], int hassasiyet) {
-    char satir[1000];
-    char satir_kopya[1000];
-    char kelime_kopya[100];
-    int kelime_sayac=0;
-    int satir_sayac=1;
+int sinir_mi(char c) { return c == '\0' || !isalnum((unsigned char)c); }
 
-    FILE *dosya = fopen(dosya_adi,"r");
-    if (dosya == NULL)
-    {
-        printf("Hata: Dosya acilamadi.\n" RENK_RESET);
-        return 1;
+int satiri_analiz_et(char *asl_satir, char *kop_satir, char *aranan, int mod,
+                     int k_len) {
+  int bulunan_adet = 0;
+  char *p = kop_satir;
+
+  while ((p = strstr(p, aranan)) != NULL) {
+    if (mod == 3) {
+      int oncesi_tam = (p == kop_satir) || sinir_mi(*(p - 1));
+      int sonrasi_tam = sinir_mi(*(p + k_len));
+      if (oncesi_tam && sonrasi_tam)
+        bulunan_adet++;
+    } else {
+      bulunan_adet++;
     }
+    p++;
+  }
+  return bulunan_adet;
+}
 
-    clock_t basla = clock();
-    strcpy(kelime_kopya, kelime);
+// Kelime arama fonksiyonu
+int kelime_bulma(char dosya_adi[100], char kelime[100], int hassasiyet,
+                 FILE *rapor) {
+  char satir[1000], satir_kopya[1000], kelime_kopya[100];
+  int kelime_sayac = 0, satir_sayac = 1;
 
-    if(hassasiyet == 2){
-        kucuk_harf(kelime_kopya); // harf duyarsız arama için küçük harfe çevir
+  FILE *dosya = fopen(dosya_adi, "r");
+  if (dosya == NULL) {
+    printf("Hata: %s dosyasi acilamadi.\n", dosya_adi);
+    fprintf(rapor, "Hata: %s dosyasi acilamadi.\n", dosya_adi);
+    return -1;
+  }
+
+  fprintf(rapor, "\n~~~DOSYA ADI: %s~~~\n", dosya_adi);
+  clock_t basla = clock();
+
+  strcpy(kelime_kopya, kelime);
+  if (hassasiyet >= 2)
+    kucuk_harf(kelime_kopya);
+  int k_len = strlen(kelime_kopya);
+
+  printf("\n" RENK_YAZI "Arama basladi...\n" RENK_RESET);
+
+  while (fgets(satir, sizeof(satir), dosya) != NULL) {
+    char *ptr;
+    strcpy(satir_kopya, satir);
+
+    if (hassasiyet >= 2)
+      kucuk_harf(satir_kopya);
+
+    int bulunan =
+        satiri_analiz_et(satir, satir_kopya, kelime_kopya, hassasiyet, k_len);
+
+    if (bulunan > 0) {
+      kelime_sayac += bulunan;
+      printf(RENK_YAZI "Satir %d:" RENK_RESET " %s", satir_sayac, satir);
+      fprintf(rapor, "Satir %d: %s", satir_sayac, satir);
     }
+    satir_sayac++;
+  }
 
-    printf("\n" RENK_YAZI "Arama basladi...\n" RENK_RESET);
+  clock_t bitir = clock();
+  double sure_ms = (double)(bitir - basla) * 1000 / CLOCKS_PER_SEC;
 
-    while (fgets(satir,sizeof(satir),dosya) != NULL) 
-    {
-        char *ptr;
-        if(hassasiyet == 2){
-            strcpy(satir_kopya, satir);
-            kucuk_harf(satir_kopya);
-            ptr = satir_kopya;
-        } else {
-            ptr = satir;
-        }
+  printf("\n" RENK_YAZI "Arama tamamlandi! \n" RENK_RESET);
+  printf("\nToplam %d adet '%s' kelimesi bulundu.\n", kelime_sayac,
+         kelime_kopya);
+  printf("Arama suresi: %.2f ms\n", sure_ms);
+  fprintf(rapor, "Toplam %d adet bulundu. Islem suresi: %.2f ms\n",
+          kelime_sayac, sure_ms);
 
-        if(strstr(ptr, kelime_kopya) != NULL){
-            printf(RENK_YAZI "Satir %d:" RENK_RESET " %s", satir_sayac, satir); 
-            kelime_sayac++; 
-           
-        }
-
-        satir_sayac++;
-    }
-
-    clock_t bitir = clock();
-    double sure_ms = (double)(bitir - basla) * 1000 / CLOCKS_PER_SEC;
-
-    printf("\n" RENK_YAZI "Arama tamamlandi! \n" RENK_RESET);
-    printf("\nToplam %d adet '%s' kelimesi bulundu.\n", kelime_sayac, kelime);
-    printf("Arama suresi: %.2f ms\n", sure_ms);
-
-    fclose(dosya);
-    return 0;
+  fclose(dosya);
+  return kelime_sayac;
 }
 
 int main() {
-    char kelime1[100];
-    char dosya_adi1[100];
-    int secim;
+  char kelime1[100], dosya_adi1[100];
+  int secim, toplam_dosya = 0, genel_toplam_eslesme = 0;
 
-    printf(RENK_YAZI "\n======= KELIME ARAMA PROGRAMI =======\n" RENK_RESET);
+  FILE *rapor_dosyasi = fopen("arama_raporu.txt", "w");
+  if (rapor_dosyasi == NULL) {
+    printf("Rapor dosyasi olusturulamadi!\n");
+    return 1;
+  }
 
-    printf("Arama yapilacak dosyanin adini giriniz:\n");
-    fgets(dosya_adi1,sizeof(dosya_adi1),stdin);
-    dosya_adi1[strcspn(dosya_adi1, "\r\n")] = 0; 
- 
-    printf("Aratmak istediginiz kelimeyi giriniz: ");
-    fgets(kelime1,100,stdin);
-    kelime1[strcspn(kelime1, "\r\n")] = 0;
-    
-     printf("\nArama tipi seciniz:\n");
-    printf("1 - Buyuk/kucuk harf DUYARLI\n");
-    printf("2 - Buyuk/kucuk harf DUYARSIZ\n");
-    printf("Seciminiz: ");
-    scanf("%d",&secim);
-    
-    kelime_bulma(dosya_adi1, kelime1, secim);
+  printf(RENK_YAZI
+         "\n~~~~~~~~~~~~ KELIME ARAMA PROGRAMI ~~~~~~~~~~~~\n" RENK_RESET);
 
-    printf(RENK_YAZI "====================================\n" RENK_RESET);
+  printf("Aratmak istediginiz kelimeyi giriniz: ");
+  fgets(kelime1, 100, stdin);
+  kelime1[strcspn(kelime1, "\r\n")] = 0;
 
-    return 0;
+  printf("\nArama tipi seciniz:\n1: Duyarli\n2: Duyarsiz\n3: Tam Kelime "
+         "(Duyarsiz)\nSecim: ");
+  if (scanf("%d", &secim) != 1)
+    return 1;
+  getchar();
+
+  while (1) {
+    printf("\nTaranacak dosya adi (Cikis icin 'exit' yazin): ");
+    fgets(dosya_adi1, sizeof(dosya_adi1), stdin);
+    dosya_adi1[strcspn(dosya_adi1, "\r\n")] = 0;
+
+    if (strcmp(dosya_adi1, "exit") == 0)
+      break;
+
+    int bulunan = kelime_bulma(dosya_adi1, kelime1, secim, rapor_dosyasi);
+
+    if (bulunan != -1) {
+      toplam_dosya++;
+      genel_toplam_eslesme += bulunan;
+    }
+  }
+
+  fprintf(rapor_dosyasi, "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+  fprintf(rapor_dosyasi, "\n           GENEL ARAMA OZETI            ");
+  fprintf(rapor_dosyasi, "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+  fprintf(rapor_dosyasi, "\nAranan Kelime:   %s", kelime1);
+  fprintf(rapor_dosyasi, "\nArama Modu:      %d", secim);
+  fprintf(rapor_dosyasi, "\nTaranan Dosya:   %d", toplam_dosya);
+  fprintf(rapor_dosyasi, "\nToplam Sonuc:    %d", genel_toplam_eslesme);
+  fprintf(rapor_dosyasi, "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+  fprintf(rapor_dosyasi, "\n--- Arama oturumu sonlandirildi ---");
+  fclose(rapor_dosyasi);
+
+  printf("\nTum sonuclar 'arama_raporu.txt' dosyasina kaydedildi.\n");
+  return 0;
 }
-
